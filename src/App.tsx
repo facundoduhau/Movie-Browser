5b220a852536e24cddc362160207099617f709ef
facundoduhau <duhaufacundo@gmail.com>
2026-08-07 21:03:06 -0300

    Working on nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { Menu } from 'lucide-react';
 
 const App = () => {
-  return (
-    <div className="w-screen min-h-screen bg-linear-to-bl from-slate-950  to-slate-900">
-      <header className="flex flex-col justify-center items-center">
-        <h1 className="w-fit text-white text-3xl">
-          Your all time classics, in your new favorite website
-        </h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="w-screen min-h-screen bg-linear-to-bl from-slate-950  to-slate-900 flex flex-col items-center">
+            <header className="text-white w-full flex flex-col items-center">
+                <div className="bg-slate-800 w-full h-16 flex items-center">
+                    <div className="flex">
+                        <Menu size={40} />
+                        <span>I'm the left part</span>
+                    </div>
+                </div>
+                <div>
+                    I'm the bottom part
+                </div>
+            </header>
+            <main className="text-white">
+                I'm Main
+            </main>
+            <footer className="text-white">
+                I'm Footer
+            </footer>
+        </div>
+    );
 };
 
 export default App;
