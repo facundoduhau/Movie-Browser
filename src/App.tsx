f945fc71d49e8cd554bcb5c8644ce84135de2fc9
facundoduhau <duhaufacundo@gmail.com>
2026-08-10 17:43:18 -0300

    Cleaned up, and fixed design of header

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,31 @@ import { Menu } from "lucide-react";
 
 const App = () => {
   return (
-    <div className="w-screen min-h-screen bg-linear-to-bl from-slate-950  to-slate-900 flex flex-col items-center">
+    <div className="w-screen h-screen bg-linear-to-bl from-slate-950  to-slate-900 flex flex-col items-center">
       <header className="text-white w-full flex flex-col items-center">
         <div className="bg-slate-800 w-full h-16 flex items-center">
-          <div className="flex">
-            <Menu size={40} />
-            <span>I'm the left part</span>
+          <div>
+            <Menu className="ml-5" size={40} />
+          </div>
+          <div className="w-full flex justify-center pr-11.25">
+            <div className="font-poppins font-extrabold text-4xl shadow-2xl">
+              📽 FILM ROLL
+            </div>
           </div>
         </div>
-        <div>I'm the bottom part</div>
       </header>
-      <main className="text-white">I'm Main</main>
-      <footer className="text-white">I'm Footer</footer>
+      <main className="text-white w-full flex flex-col">
+        <div className="justify-center text-center mt-5 mb-5">
+          <span className="text-2xl font-poppins">
+            Your{" "}
+            <span className="italic font-bold text-violet-200">favorite</span>{" "}
+            films. In your new{" "}
+            <span className="italic font-bold text-violet-200">favorite</span>{" "}
+            streaming site.
+          </span>
+        </div>
+      </main>
+      <footer className="text-white bg-green-600 w-full h-8">I'm Footer</footer>
     </div>
   );
 };
