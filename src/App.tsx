db880ee877bb2a34e9afc3639984907fdfc8e9a5
facundoduhau <duhaufacundo@gmail.com>
2026-08-11 23:51:52 -0300

    ENV Variables and start of API implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  header: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 const App = () => {
   return (
     <div className="w-screen h-screen bg-linear-to-bl from-slate-950  to-slate-900 flex flex-col items-center">
